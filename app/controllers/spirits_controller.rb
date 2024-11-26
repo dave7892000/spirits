69ee09481f073380aef3d49fa38bd0c3760c27e3
David Steketee <david.steketee@gmail.com>
2024-11-26 12:02:02 -0500

    initial crud setup

---
+++ app/controllers/spirits_controller.rb
@@ -0,0 +1,58 @@
+class SpiritsController < ApplicationController
+  before_action :set_spirit, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @spirits = Spirit.all
+  end
+
+  def new
+    @spirit = Spirit.new
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+  
+  def create
+    @spirit = Spirit.new(spirit_params)
+    if @spirit.save
+      flash[:notice] = "Spirit was created successfully"
+      redirect_to spirits_path
+    else render :new, status: :unprocesable_entity
+    end
+  end
+
+  def update
+    if @spirit.update(spirit_params)
+      flash[:notice] = "Spirit was updated successfully"
+      redirect_to spirits_path
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @spirit.destroy
+    redirect_to spirits_path
+  end
+
+  private
+
+  def spirit_params
+    (params.require(:spirit).permit(:name, :abv, :spirit_type))
+  end
+
+  def set_spirit
+    @spirit = Spirit.find(params[:id])
+  end
+
+
+end
+
+
+
+
