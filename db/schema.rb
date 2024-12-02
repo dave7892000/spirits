16f40a505d37411968d3c261ce0deca1754d5b6b
David Steketee <david.steketee@gmail.com>
2024-12-02 15:13:36 -0500

    create users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_26_144606) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_02_200815) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_26_144606) do
     t.string "name"
     t.decimal "abv"
     t.string "spirit_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
